ab95a2953a908d09ab7806d08956f8dbbaaa3904
Tarun Pal <139231055+TarunPalTech@users.noreply.github.com>
2026-02-28 18:06:26 +0000

    working...

--- Assignment-1/program-7.cpp
+++ Assignment-1/program-7.cpp
@@ -1,3 +1,20 @@
 /* 
 Problem-7: Write a C++ program  to calculate the square of a number.
 */
+
+#include<iostream>
+using namespace std;
+
+inline float calculateSquare(int);
+
+int main(){
+    float n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    cout<<"Square is "<<calculateSquare(n)<<"!";
+    return 0;
+}
+
+float calculateSquare(int n){
+    return n*n;
+}
