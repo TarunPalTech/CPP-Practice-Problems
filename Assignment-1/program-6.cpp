3633ceeb9753a7236a1acb14f8d6127674727b8a
Tarun Pal <139231055+TarunPalTech@users.noreply.github.com>
2026-02-28 17:56:38 +0000

    working...

--- Assignment-1/program-6.cpp
+++ Assignment-1/program-6.cpp
@@ -3,9 +3,18 @@ Problem-3: Write a C++ program to calculate average of 3 numbers.
 */
 
 #include<iostream>
-
 using namespace std;
 
+float average(float a, float b, float c);
+
 int main(){
-    
+    float a,b,c;
+    cout<<"Enter three values: ";
+    cin>>a>>b>>c;
+    cout<<"Average is "<<average(a,b,c)<<"!";
+    return 0;
+}
+
+float average(float a, float b, float c){
+    return (a+b+c)/3.0;
 }
