cd934894510d41f5e168ee71eceb4f8dabaf3bd5
Tarun Pal <139231055+TarunPalTech@users.noreply.github.com>
2026-06-14 06:10:50 +0000

    first commit

--- Assignment-4(Classes and Objects)/program-1.cpp
+++ Assignment-4(Classes and Objects)/program-1.cpp
@@ -1,3 +1,6 @@
+// Problem - 1:- Define a class Complex to represent a complex number. Declare instance member variables to store real and imaginary part of a complex number, also define instance member functions to set values of complex number and print values of complex number.
+
+
 #include<iostream>
 using namespace std;
 
